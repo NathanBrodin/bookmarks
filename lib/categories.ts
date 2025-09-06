e1833f8435dd747fe99fa3545010f53f7bf92f68
Nathan Brodin <nathan@brodin.dev>
2025-09-06 16:22:07 +0200

    Update categories with new labels and color classes

--- lib/categories.ts
+++ lib/categories.ts
@@ -6,57 +6,57 @@ export type Category = {
 
 export const categories: Category[] = [
   {
-    label: "Design",
-    value: "design",
+    label: "Inspirations",
+    value: "Inspirations",
     classlabel:
       "border-purple-300 bg-purple-200/10 text-purple-900 dark:border-purple-400/15 dark:bg-purple-800/5 dark:text-purple-200",
   },
   {
-    label: "Components",
-    value: "components",
+    label: "Registries",
+    value: "registries",
     classlabel:
       "border-blue-300 bg-blue-200/10 text-blue-900 dark:border-blue-400/15 dark:bg-blue-800/5 dark:text-blue-200",
   },
   {
-    label: "React",
-    value: "react",
+    label: "Portfolios",
+    value: "portfolios",
     classlabel:
       "border-cyan-300 bg-cyan-200/10 text-cyan-900 dark:border-cyan-400/15 dark:bg-cyan-800/5 dark:text-cyan-200",
   },
   {
-    label: "CSS",
-    value: "css",
+    label: "Tools",
+    value: "Tools",
     classlabel:
       "border-orange-300 bg-orange-200/10 text-orange-900 dark:border-orange-400/15 dark:bg-orange-800/5 dark:text-orange-200",
   },
   {
-    label: "Utility",
-    value: "utility",
+    label: "Articles",
+    value: "Articles",
     classlabel:
       "border-green-300 bg-green-200/10 text-green-900 dark:border-green-400/15 dark:bg-green-800/5 dark:text-green-200",
   },
   {
-    label: "Development",
-    value: "development",
+    label: "Services",
+    value: "Services",
     classlabel:
-      "border-gray-300 bg-gray-200/10 text-gray-900 dark:border-gray-400/15 dark:bg-gray-800/5 dark:text-gray-200",
+      "border-teal-300 bg-teal-200/10 text-teal-900 dark:border-teal-400/15 dark:bg-teal-800/5 dark:text-teal-200",
   },
   {
-    label: "Tools",
-    value: "tools",
+    label: "Templates",
+    value: "templates",
+    classlabel:
+      "border-fuchsia-300 bg-fuchsia-200/10 text-fuchsia-900 dark:border-fuchsia-400/15 dark:bg-fuchsia-800/5 dark:text-fuchsia-200",
+  },
+  {
+    label: "My Stuff",
+    value: "my-stuff",
     classlabel:
       "border-yellow-300 bg-yellow-200/10 text-yellow-900 dark:border-yellow-400/15 dark:bg-yellow-800/5 dark:text-yellow-200",
   },
   {
-    label: "Git",
-    value: "git",
+    label: "Others",
+    value: "Others",
     classlabel:
-      "border-red-300 bg-red-200/10 text-red-900 dark:border-red-400/15 dark:bg-red-800/5 dark:text-red-200",
-  },
-  {
-    label: "Inspiration",
-    value: "inspiration",
-    classlabel:
-      "border-pink-300 bg-pink-200/10 text-pink-900 dark:border-pink-400/15 dark:bg-pink-800/5 dark:text-pink-200",
+      "border-gray-300 bg-gray-200/10 text-gray-900 dark:border-gray-400/15 dark:bg-gray-800/5 dark:text-gray-200",
   },
 ]
